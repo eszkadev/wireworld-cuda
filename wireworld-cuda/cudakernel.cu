d075aec028371e2414ba9b1e1a105be24c0c53ef
Szymon Kłos <eszkadev@gmail.com>
2017-05-31 18:11:24 +0200

    Multi GPU support

--- wireworld-cuda/cudakernel.cu
+++ wireworld-cuda/cudakernel.cu
@@ -7,14 +7,14 @@
 int BLOCK_SIZE = 32;
 int CELLS_PER_THREAD = 8;
 int GRID_SIZE = 16;
-int USE_GPU[MAX_GPUS];
-int CURRENT_GPU[MAX_GPUS];
 
 #define PART_SIZE BLOCK_SIZE * CELLS_PER_THREAD * GRID_SIZE
 
-Map d_pMap = NULL;
-Map d_pNewMap = NULL;
-cudaStream_t stream;
+int USE_GPU[MAX_GPUS];
+int CURRENT_GPU[MAX_GPUS];
+Map d_pMap[MAX_GPUS];
+Map d_pNewMap[MAX_GPUS];
+cudaStream_t stream[MAX_GPUS];
 
 extern "C" DevicesInfo* CUDA_getDevicesList()
 {
@@ -102,21 +102,14 @@ __global__ void step(Map pOld, Map pNew, unsigned int nWidth, unsigned int nHeig
 
 extern "C" void CUDA_setup()
 {
-    cudaFree(d_pMap);
-    cudaFree(d_pNewMap);
-    if(stream)
-        cudaStreamDestroy(stream);
+    int nThreadId = omp_get_thread_num();
+
+    cudaFree(d_pMap[nThreadId]);
+    cudaFree(d_pNewMap[nThreadId]);
 
     cudaError_t aError = cudaSuccess;
 
-    aError = cudaStreamCreate(&stream);
-    if(aError != cudaSuccess)
-    {
-        fprintf(stderr, "Failed to create cuda stream: %d!\n", cudaGetErrorString(aError));
-        exit(EXIT_FAILURE);
-    }
-
-    aError = cudaMalloc((void**)&d_pMap, (PART_SIZE + 2) * (PART_SIZE + 2) * sizeof(Cell));
+    aError = cudaMalloc((void**)&d_pMap[nThreadId], (PART_SIZE + 2) * (PART_SIZE + 2) * sizeof(Cell));
 
     if(aError != cudaSuccess)
     {
@@ -124,7 +117,7 @@ extern "C" void CUDA_setup()
         exit(EXIT_FAILURE);
     }
 
-    aError = cudaMalloc((void**)&d_pNewMap, (PART_SIZE + 2) * (PART_SIZE + 2) * sizeof(Cell));
+    aError = cudaMalloc((void**)&d_pNewMap[nThreadId], (PART_SIZE + 2) * (PART_SIZE + 2) * sizeof(Cell));
 
     if(aError != cudaSuccess)
     {
@@ -135,16 +128,14 @@ extern "C" void CUDA_setup()
 
 extern "C" void CUDA_exit()
 {
-    cudaFree(d_pMap);
-    d_pMap = NULL;
-    cudaFree(d_pNewMap);
-    d_pNewMap = NULL;
-    cudaStreamDestroy(stream);
-    stream = 0;
-    cudaDeviceReset();
+    for(int i = 0; i < MAX_GPUS; i++)
+    {
+        cudaFree(d_pMap[i]);
+        cudaFree(d_pNewMap[i]);
+    }
 }
 
-void CUDA_step_big_data(Model *pModel, int n)
+void CUDA_step_big_data(Model *pModel, int n, int nThreadOrder)
 {
     cudaError_t aError = cudaSuccess;
 
@@ -156,7 +147,11 @@ void CUDA_step_big_data(Model *pModel, int n)
     {
         for(int nY = 0; nY < nHeight || nY == 0; nY += PART_SIZE)
         {
-            for(int nX = 0; nX < nWidth || nX == 0; nX += PART_SIZE)
+            int nThreadId = omp_get_thread_num();
+            int nThreads = omp_get_num_threads();
+            int nStart = 0 + nThreadOrder * PART_SIZE;
+
+            for(int nX = nStart; nX < nWidth || nX == 0; nX += nThreads * PART_SIZE)
             {
                 int nLength = (nHeight - nY > PART_SIZE) ? PART_SIZE + 2 : nHeight - nY + 2;
                 int nRows = (nWidth - nX > PART_SIZE) ? PART_SIZE + 2 : nWidth - nX + 2;
@@ -167,45 +162,45 @@ void CUDA_step_big_data(Model *pModel, int n)
                 int nRowDevice = 0;
                 for(int nRow = nX; nRowDevice < nRows; nRow += nHeight)
                 {
-                    aError = cudaMemcpyAsync(d_pMap + (nRowDevice++) * nLength, pMap + nRow + nY, nLength * sizeof(Cell), cudaMemcpyHostToDevice, stream);
+                    aError = cudaMemcpyAsync(d_pMap[nThreadId] + (nRowDevice++) * nLength, pMap + nRow + nY, nLength * sizeof(Cell), cudaMemcpyHostToDevice, stream[nThreadId]);
                     if(aError != cudaSuccess)
                     {
-                        fprintf(stderr, "Line: %d, Failed to copy Map[%d,%d,%d] from device to host (error code %s)!\n", __LINE__, nX, nY, nRow, cudaGetErrorString(aError));
-                        exit(EXIT_FAILURE);
+                        fprintf(stderr, "GPU %d: Line: %d, Failed to copy Map[%d,%d,%d] from device to host (error code %s)!\n", nThreadId, __LINE__, nX, nY, nRow, cudaGetErrorString(aError));
+                        return;
                     }
                 }
 
                 dim3 blockDim(BLOCK_SIZE, BLOCK_SIZE, 1);
                 dim3 gridDim(GRID_SIZE, GRID_SIZE, 1);
-                printf("CUDA big kernel launch with %dx%d blocks of %dx%d threads\n", gridDim.x, gridDim.y, blockDim.x, blockDim.y);
-                fflush(stdout);
+                printf("GPU %d: CUDA big kernel launch with %dx%d blocks of %dx%d threads\n", nThreadId, gridDim.x, gridDim.y, blockDim.x, blockDim.y);
 
-                step<<<gridDim, blockDim, 0, stream>>>(d_pMap, d_pNewMap, nRows, nLength, CELLS_PER_THREAD);
+                step<<<gridDim, blockDim, 0, stream[nThreadId]>>>(d_pMap[nThreadId], d_pNewMap[nThreadId], nRows, nLength, CELLS_PER_THREAD);
 
                 aError = cudaGetLastError();
 
                 if(aError != cudaSuccess)
                 {
-                    fprintf(stderr, "Failed to launch step kernel (error code %s)!\n", cudaGetErrorString(aError));
-                    exit(EXIT_FAILURE);
+                    fprintf(stderr, "GPU %d: Failed to launch step kernel (error code %s)!\n", nThreadId, cudaGetErrorString(aError));
+                    return;
                 }
 
                 nRowDevice = 1;
                 for(int nRow = nX; nRowDevice < nRows - 1; nRow += nHeight)
                 {
-                    aError = cudaMemcpyAsync(pMap + 1 + nHeight + nRow + nY, d_pNewMap + 1 + (nRowDevice++) * nLength, (nLength - 2) * sizeof(Cell), cudaMemcpyDeviceToHost, stream);
+                    aError = cudaMemcpyAsync(pMap + 1 + nHeight + nRow + nY, d_pNewMap[nThreadId] + 1 + (nRowDevice++) * nLength, (nLength - 2) * sizeof(Cell), cudaMemcpyDeviceToHost, stream[nThreadId]);
                     if(aError != cudaSuccess)
                     {
-                        fprintf(stderr, "Line: %d, Failed to copy Map[%d,%d,%d] from device to host (error code %s)!\n", __LINE__, nX, nY, nRow, cudaGetErrorString(aError));
-                        exit(EXIT_FAILURE);
+                        fprintf(stderr, "GPU %d: Line: %d, Failed to copy Map[%d,%d,%d] from device to host (error code %s)!\n", nThreadId, __LINE__, nX, nY, nRow, cudaGetErrorString(aError));
+                        return;
                     }
                 }
             }
         }
+        //#pragma omp barrier
     }
 }
 
-void CUDA_step_small_data(Model* pModel, int n)
+void CUDA_step_small_data(Model* pModel, int n, int nGPU)
 {
     cudaError_t aError = cudaSuccess;
 
@@ -213,43 +208,42 @@ void CUDA_step_small_data(Model* pModel, int n)
     int nHeight = pModel->GetHeight() + 2;
     Map pMap = pModel->GetMap();
 
-    aError = cudaMemcpyAsync(d_pMap, pMap, nHeight * nWidth * sizeof(Cell), cudaMemcpyHostToDevice, stream);
+    aError = cudaMemcpyAsync(d_pMap[nGPU], pMap, nHeight * nWidth * sizeof(Cell), cudaMemcpyHostToDevice, stream[nGPU]);
     if(aError != cudaSuccess)
     {
         fprintf(stderr, "Line: %d, Failed to copy Map from device to host (error code %s)!\n", __LINE__, cudaGetErrorString(aError));
-        exit(EXIT_FAILURE);
+        return;
     }
 
     dim3 blockDim(BLOCK_SIZE, BLOCK_SIZE, 1);
     dim3 gridDim(GRID_SIZE, GRID_SIZE, 1);
     printf("CUDA small kernel launch with %dx%d blocks of %dx%d threads\n", gridDim.x, gridDim.y, blockDim.x, blockDim.y);
-    fflush(stdout);
 
     for(int i = 0; i < n; ++i)
     {
-        step<<<gridDim, blockDim, 0, stream>>>(d_pMap, d_pNewMap, nWidth, nHeight, CELLS_PER_THREAD);
+        step<<<gridDim, blockDim, 0, stream[nGPU]>>>(d_pMap[nGPU], d_pNewMap[nGPU], nWidth, nHeight, CELLS_PER_THREAD);
 
         aError = cudaGetLastError();
 
         if(aError != cudaSuccess)
         {
             fprintf(stderr, "Failed to launch step kernel (error code %s)!\n", cudaGetErrorString(aError));
-            exit(EXIT_FAILURE);
+            return;
         }
 
         if(i < n - 1)
         {
-            Map pTmp = d_pMap;
-            d_pMap = d_pNewMap;
-            d_pNewMap = pTmp;
+            Map pTmp = d_pMap[nGPU];
+            d_pMap[nGPU] = d_pNewMap[nGPU];
+            d_pNewMap[nGPU] = pTmp;
         }
     }
 
-    aError = cudaMemcpyAsync(pMap, d_pNewMap, nHeight * nWidth * sizeof(Cell), cudaMemcpyDeviceToHost, stream);
+    aError = cudaMemcpyAsync(pMap, d_pNewMap[nGPU], nHeight * nWidth * sizeof(Cell), cudaMemcpyDeviceToHost, stream[nGPU]);
     if(aError != cudaSuccess)
     {
         fprintf(stderr, "Line: %d, Failed to copy Map from device to host (error code %s)!\n", __LINE__, cudaGetErrorString(aError));
-        exit(EXIT_FAILURE);
+        return;
     }
 }
 
@@ -257,9 +251,33 @@ extern "C" int CUDA_step(Model* pModel, int n)
 {
     int nWidth = pModel->GetWidth() + 2;
     int nHeight = pModel->GetHeight() + 2;
+    int nThreadOrder = 0;
 
     if(nWidth > PART_SIZE || nHeight > PART_SIZE)
-        CUDA_step_big_data(pModel, n);
+    {
+        #pragma omp parallel num_threads(MAX_GPUS)
+        {
+            int nThreadId = omp_get_thread_num();
+            if(USE_GPU[nThreadId])
+            {
+                cudaSetDevice(nThreadId);
+
+                cudaError_t aError = cudaStreamCreate(&stream[nThreadId]);
+                if(aError != cudaSuccess)
+                    fprintf(stderr, "Failed to create cuda stream: %d!\n", cudaGetErrorString(aError));
+
+                if(CURRENT_GPU[nThreadId] != USE_GPU[nThreadId])
+                {
+                    CUDA_setup();
+                    CURRENT_GPU[nThreadId] = USE_GPU[nThreadId];
+                }
+
+                CUDA_step_big_data(pModel, n, nThreadOrder++);
+
+                cudaStreamDestroy(stream[nThreadId]);
+            }
+        }
+    }
     else
     {
         int nGPU = -1;
@@ -271,16 +289,25 @@ extern "C" int CUDA_step(Model* pModel, int n)
                 break;
             }
         }
+
         if(nGPU >= 0)
         {
             printf("Use GPU %d\n", nGPU);
+
             if(CURRENT_GPU[nGPU] != USE_GPU[nGPU])
             {
                 cudaSetDevice(nGPU);
                 CUDA_setup();
                 CURRENT_GPU[nGPU] = USE_GPU[nGPU];
             }
-            CUDA_step_small_data(pModel, n);
+
+            cudaError_t aError = cudaStreamCreate(&stream[nGPU]);
+            if(aError != cudaSuccess)
+                fprintf(stderr, "Failed to create cuda stream: %d!\n", cudaGetErrorString(aError));
+
+            CUDA_step_small_data(pModel, n, nGPU);
+
+            cudaStreamDestroy(stream[nGPU]);
         }
         else
         {
@@ -305,8 +332,5 @@ extern "C" void CUDA_set(int nCells, int nBlock, int nGrid, int* pGPU)
         USE_GPU[pGPU[i]] = 1;
     }
 
-    CUDA_setup();
-
     printf("CUDA settings changed\n");
-    fflush(stdout);
 }
